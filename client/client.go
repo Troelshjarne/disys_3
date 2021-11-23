82eb7e9f764e77c5c25c8f8757d9524431ecc32f
Philip <phfi@itu.dk>
2021-11-23 16:16:44 +0100

    client progress

--- client/client.go
+++ client/client.go
@@ -2,46 +2,65 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
+
+	"github.com/Troelshjarne/disys_3/auction"
 
 	"google.golang.org/grpc"
 )
 
 var userName = flag.String("user", "default", "Users name")
-var tcpServer = flag.String("server", ":9080", "Tcp server")
+var tcpServer = flag.String("server", ":9080", "TCP Server")
+
+var options []grpc.DialOption
+
+var ctx = context.Background()
+
+// IPs to all known replicas
+var ips []string
 
 func main() {
 
-	var options []grpc.DialOption
+	//Dial options
 	options = append(options, grpc.WithBlock(), grpc.WithInsecure())
 
-	conn, err := grpc.Dial(*tcpServer, options...)
-	if err != nil {
-		log.Fatalf("Failed to dial: %v", err)
-	}
-
-	defer conn.Close()
-
-	//ctx := context.Background()
-
-	//client := auction.NewCommunicationClient(conn)
+	//Get ips
+	ips = append(ips, *tcpServer)
+	getIps()
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		text := scanner.Text()
 
+		//Sending different requests to replicas depedent on string contents
 		if typeOfRequest(text) == "bid" {
-			//Send bid here with appropriate method ()
-			//Proposed format example: bid 2000 "userName"
+			//Proposed format example: bid 2000
+
+			//Extracting bid from input
+			textSlice := strings.Split(text, " ")
+			bidAmount, err := strconv.Atoi(textSlice[1])
+			if err != nil {
+				fmt.Printf("Error converting string to int %v \n", err)
+			}
+
+			//Making the bid
+			bid(*userName, int32(bidAmount))
+
 		} else if typeOfRequest(text) == "status" {
-			//Send status request with appropriate method here ()
 			//Proposed format example: status
+
+			//Send status request
+			statusRequest()
 		} else {
 			//Print msg to client stating that their message wasn't understood.
-			//Proposed format example: Error in message - Did you mean to bid, type "xxx" or request status, type "yyy"
+			log.Println("Error in your input: \"%v\" \n - Did you mean to bid, type \"bid *amount*\" \n - Did you mean to request auction status, type \"status\"", text)
+
 		}
 	}
 
@@ -58,10 +77,62 @@ func typeOfRequest(input string) string {
 	}
 }
 
+func askForIps(ip string) (bool, []string) {
+	conn, err := grpc.Dial(ip, options...)
+	if err != nil {
+		log.Printf("Failed to connect to %s\n", ip)
+		return false, nil
+	}
+	defer conn.Close()
+	client := auction.NewCommunicationClient(conn)
+	msg, _ := client.GetReplicas(ctx, &auction.Void{})
+
+	return true, msg.Ips
+}
+
+func getIps() {
+	for _, ip := range ips {
+		if success, newIps := askForIps(ip); success {
+			// Overwrite ips.
+			ips = newIps
+			// Exit function after first successful answer.
+			return
+		}
+	}
+	// Failed to get ips from any replicas; initial connect failed or network is down.
+	log.Fatalf("Failed to retrieve ips from all known ips.\n")
+}
+
 func bid(userName string, amount int32) {
+
+	for _, ip := range ips {
+		go func() {
+			conn, err := grpc.Dial(*tcpServer, options...)
+			if err != nil {
+				log.Fatalf("Failed to connect to IP: %v", ip)
+				return
+			}
+			defer conn.Close()
+			client := auction.NewCommunicationClient(conn)
+			client.Bid(ctx, &auction.BidMessage{Amount: amount, ClientName: userName})
+		}()
+	}
 
 }
 
-func statusRequest(input string) {
+func statusRequest() {
+
+	for _, ip := range ips {
+		go func() {
+			conn, err := grpc.Dial(*tcpServer, options...)
+			if err != nil {
+				log.Fatalf("Failed to connect to IP: %v", ip)
+				return
+			}
+			defer conn.Close()
+			client := auction.NewCommunicationClient(conn)
+			client.Result(ctx, &auction.Void{})
+		}()
+	}
 
 }
