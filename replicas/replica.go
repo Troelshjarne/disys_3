759002f5e84978225bf4c97c374bf918c7924f6f
troels <trhj@itu.dk>
2021-11-23 14:06:18 +0100

    .

--- replicas/replica.go
+++ replicas/replica.go
@@ -9,11 +9,47 @@ import (
 	"google.golang.org/grpc"
 )
 
+var highestBid = 0
+var ongoing = true
+var winningClient = ""
+
 type Server struct {
 	auctionPackage.UnimplementedCommunicationServer
 
 	//Map to store channel pointers. These are clients connecting to the service.
 	//channel map[string][]chan *chatpackage.ChatMessage
+}
+
+func (s *Server) Result() {
+
+	// create resultmessage and send to client
+	resultMessage := auctionPackage.ResultMessage{
+		HighestBid:    int32(highestBid),
+		Ongoing:       ongoing,
+		WinningClient: winningClient,
+	}
+	return resultMessage
+}
+
+func (s *Server) Bid() {
+	ack := auctionPackage.MessageAck{
+		Status: "Bid accepted",
+	}
+
+	return ack
+
+}
+
+func (s *Server) StartAuction() {
+
+}
+
+func (s *Server) EndAuction() {
+
+}
+
+func (s *Server) GetReplicas() {
+
 }
 
 func main() {
@@ -28,8 +64,5 @@ func main() {
 	var options []grpc.ServerOption
 	grpcServer := grpc.NewServer(options...)
 
-	//auctionPackage.RegisterCommunicationServer(grpcServer, &Server{
-	//	channel: make(map[string][]chan *auctionPackage.Request),
-	//})
 	grpcServer.Serve(list)
 }
