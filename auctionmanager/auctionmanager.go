482e4db7615fbc742bd58e1c95505119578b7e46
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2021-11-23 14:57:20 +0100

    Auction Manager finished.

--- auctionmanager/auctionmanager.go
+++ auctionmanager/auctionmanager.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"log"
-	"os"
 	"time"
+
+	"github.com/Troelshjarne/disys_3/auction"
+
+	"google.golang.org/grpc"
 )
 
+// Context
+var ctx = context.Background()
+// Initial ip to connect to. Gets further connections from here.
+var seedIp = flag.String("server", ":9080", "TCP Server")
+var options []grpc.DialOption
+
+// IPs to all known replicas
 var ips []string
+
+func askForIps(ip string) (bool, []string) {
+	conn, err := grpc.Dial(ip, options...)
+	if err != nil {
+		log.Printf("Failed to connect to %s\n", ip)
+		return false, nil
+	}
+	defer conn.Close()
+	client := auction.NewCommunicationClient(conn)
+	msg, _ := client.GetReplicas(ctx, &auction.Void{})
+
+	return true, msg.Ips
+}
 
 func getIps() {
 	for _, ip := range ips {
-		// TODO: query replica for replicas in network.
-		if ip != "" {
+		if success, newIps := askForIps(ip); success {
+			// Overwrite ips.
+			ips = newIps
+			// Exit function after first successful answer.
 			return
 		}
 	}
@@ -25,6 +52,18 @@ func startAuction() {
 
 	// TODO: send message to all clients, to start a new auction.
 	log.Printf("Starting auction.\n")
+	for _, ip := range ips {
+		go func() {
+			conn, err := grpc.Dial(ip, options...)
+			if err != nil {
+				log.Printf("Failed to connect to %s\n", ip)
+				return
+			}
+			defer conn.Close()
+			client := auction.NewCommunicationClient(conn)
+			client.StartAuction(ctx, &auction.Void{})
+		}()
+	}
 }
 
 func endAuction() {
@@ -33,12 +72,29 @@ func endAuction() {
 
 	// TODO: send message to all clients, that the auction is over.
 	log.Printf("Ending auction.\n")
+	for _, ip := range ips {
+		go func() {
+			conn, err := grpc.Dial(ip, options...)
+			if err != nil {
+				log.Printf("Failed to connect to %s\n", ip)
+				return
+			}
+			defer conn.Close()
+			client := auction.NewCommunicationClient(conn)
+			client.EndAuction(ctx, &auction.Void{})
+		}()
+	}
 }
 
 func main() {
-	ips = append(ips, os.Args[1])
+	// Add dial options.
+	options = append(options, grpc.WithBlock(), grpc.WithInsecure())
+
+	// Add seed ip to list and try to get it propagate further ips.
+	ips = append(ips, *seedIp)
 	getIps()
 
+	// Run auctions for 2 minutes, end them, and start another auction after 15 secs.
 	for {
 		time.Sleep(time.Minute*2)
 		endAuction()
