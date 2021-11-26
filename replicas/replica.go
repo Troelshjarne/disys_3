f6c8e25787849bd63be45c1c793bd132507e6111
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2021-11-26 01:59:41 +0100

    Added Serf functionality for maintaining node clusters.

--- replicas/replica.go
+++ replicas/replica.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/hashicorp/serf/serf"
 	"log"
 	"net"
+	"strings"
 	"sync"
+	"time"
 
 	auctionPackage "github.com/Troelshjarne/disys_3/auction"
 	"google.golang.org/grpc"
@@ -14,6 +18,11 @@ var Mutex sync.Mutex
 var highestBid = 0
 var ongoing = true
 var winningClient = ""
+
+var port = flag.Int("port", 9080, "Port to connect to")
+var clusterTarget = flag.String("target", "", "Target node in Serf cluster to join. Ex 127.0.0.1:9080")
+
+var serfClient *serf.Serf
 
 type Server struct {
 	auctionPackage.UnimplementedCommunicationServer
@@ -81,16 +90,22 @@ func (s *Server) EndAuction(void auctionPackage.Void) auctionPackage.Void {
 }
 
 func (s *Server) GetReplicas(void auctionPackage.Void) auctionPackage.IpMessage {
+	var ips []string
+
+	for _, member := range serfClient.Members() {
+		ips = append(ips, fmt.Sprintf("%s:%d", member.Addr, member.Port))
+	}
 
 	return auctionPackage.IpMessage{
-		Ips: nil,
+		Ips: ips,
 	}
 }
 
 func main() {
+	flag.Parse()
 
 	fmt.Println("=== Replica node starting up ===")
-	list, err := net.Listen("tcp", ":9080")
+	list, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 
 	if err != nil {
 		log.Fatalf("Failed to listen on port 9080: %v", err)
@@ -99,5 +114,80 @@ func main() {
 	var options []grpc.ServerOption
 	grpcServer := grpc.NewServer(options...)
 
+	// Serf set-up
+	serfCh := make(chan serf.Event)
+	serfClient, err = serf.Create(getSerfConfig(port, &serfCh))
+	if err != nil {
+		log.Fatalf("Failed to create Serf client. Error: %v", err)
+	}
+	defer serfClient.Shutdown()
+
+	if len(*clusterTarget) != 0 {
+		log.Printf("Attempting to join Serf cluster on IP: %s\n", *clusterTarget)
+		memCount, err := serfClient.Join(
+			// Join "Replicant:{port}/{ip}"
+			[]string{ fmt.Sprintf("Replicant:%d/%s", strings.Split(*clusterTarget, ":")[1], clusterTarget) },
+			false)
+		if err != nil {
+			log.Fatalf("Failed to join Serf cluster. Error: %v", err)
+		} else {
+			log.Printf("Serf cluster found! Joined cluster of %d clients.", memCount)
+		}
+	} else {
+		log.Printf("Establishing independant Serf cluster on 127.0.0.1:%d\n", *port)
+	}
+	go announceSerfEvents(&serfCh)
+	go announceSerfMemberList(serfClient)
+	// Serf set-up complete.
+
 	grpcServer.Serve(list)
+}
+
+func announceSerfEvents(ch *chan serf.Event) {
+	for {
+		event := <- *ch
+		log.Printf("Received Serf event: %v", event)
+	}
+}
+
+func announceSerfMemberList(client *serf.Serf) {
+	for {
+		time.Sleep(time.Second*10)
+		log.Printf("Current Serf cluster size: %d\nMembers:", client.NumNodes())
+		for _, mem := range client.Members() {
+			log.Printf("\t%s | %s:%d\n", mem.Name, mem.Addr, mem.Port)
+		}
+	}
+}
+
+func getSerfConfig(port *int, eventCh *chan serf.Event) *serf.Config {
+	conf := serf.DefaultConfig()
+	conf.Init()
+
+	conf.MemberlistConfig.BindAddr = "127.0.0.1"
+	conf.MemberlistConfig.BindPort = *port
+	conf.NodeName = fmt.Sprintf("Replicant:%d", port)
+	conf.ValidateNodeNames = false
+
+	// Set probe intervals that are aggressive for finding bad nodes
+	conf.MemberlistConfig.GossipInterval = 50 * time.Millisecond
+	conf.MemberlistConfig.ProbeInterval = 500 * time.Millisecond
+	conf.MemberlistConfig.ProbeTimeout = 250 * time.Millisecond
+	conf.MemberlistConfig.TCPTimeout = 1000 * time.Millisecond
+	conf.MemberlistConfig.SuspicionMult = 1
+
+	// Set a short reap interval so that it can run during the test
+	conf.ReapInterval = 10 * time.Millisecond
+
+	// Set a short reconnect interval so that it can run a lot during tests
+	conf.ReconnectInterval = 10 * time.Millisecond
+
+	// Set basically zero on the reconnect/tombstone timeouts so that
+	// they're removed on the first ReapInterval.
+	conf.ReconnectTimeout = 1 * time.Millisecond
+	conf.TombstoneTimeout = 1 * time.Millisecond
+
+	conf.EventCh = *eventCh
+
+	return conf
 }
